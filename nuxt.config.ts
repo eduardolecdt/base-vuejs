792cd2d67d2664284b5fe2fe79af3eaa9eec861c
Edu Sites <eduardolecdt@gmail.com>
2025-04-28 23:49:41 -0300

    base projeto vuejs 3.0 com API composition

--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: false,
-  components: true,
-  css: ['~/assets/css/index.css'],
-  modules: ['@pinia/nuxt']
-})
--- pages/erro/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Nav />
-  <SectionApresentacao />
-  <Footer />
-</template>
-
-<script setup>
-import SectionApresentacao from '@/components/pages/erro/SectionApresentacao.vue'
-</script>
